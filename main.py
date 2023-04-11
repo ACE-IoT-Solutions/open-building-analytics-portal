196f495a12689b99f0f058b1635d977140f9964c
Andrew <andrew@aceiotsolutions.com>
2023-04-11 10:14:29 -0400

    Added requirements hardcoded hack for date column

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ samples = st.file_uploader("Upload a CSV file", type="csv")
 column_mappings = {}
 parameters = {}
 if samples is not None:
-    dataframe = pd.read_csv(samples)
+    dataframe = pd.read_csv(samples, index_col="Date", parse_dates=True).rolling("5T").mean()
     st.write(dataframe)
     for input in (input for input in inputs.parameters if input.endswith("_col")):
         column_mappings[input] = st.selectbox(
@@ -44,11 +44,26 @@ if samples is not None:
         res = fault_map[rule_to_check](**column_mappings, **parameters).apply(dataframe)
         st.write(res)
     report_name = st.text_input("Report Name")
+    report_def = report_map[f'{rule_to_check.replace("Condition", "Code")}Report']
+    report_args = inspect.signature(report_def).parameters
+    filtered_report_args = {key: value for key, value in report_args.items() if key not in dict(**column_mappings, **parameters)}
+    for report_input in (report_input for report_input in filtered_report_args if report_input.endswith("_col")):
+        column_mappings[report_input] = st.selectbox(
+            f"Input for {rule_to_check} - {report_input}", [col for col in dataframe.columns]
+        )
+        st.write(column_mappings[report_input])
+    for report_input, p in {
+        report_input: p for report_input, p in filtered_report_args.items() if not report_input.endswith("_col")
+    }.items():
+        parameters[report_input] = input_map[p.annotation](
+            f"Input for {rule_to_check} - {report_input}"
+        )
+        st.write(parameters[report_input])
     if st.button("Run Report"):
-        res = fault_map[rule_to_check](**column_mappings, **parameters).apply(dataframe)
-        report_def = report_map[f'{rule_to_check.replace("Condition", "Code")}Report']
-        filtered_args = {key: value for key, value in column_mappings.items() if key in inspect.signature(report_def).parameters}
-        report = report_def(**filtered_args).create_report(report_name, res)
+        filtered_fault_args = {key: value for key, value in dict(**column_mappings, **parameters).items() if key in inputs.parameters}
+        res = fault_map[rule_to_check](**filtered_fault_args).apply(dataframe)
+        filtered_report_args = {key: value for key, value in column_mappings.items() if key in report_args}
+        report = report_def(**filtered_report_args).create_report(report_name, res)
         download_file = BytesIO()
         report.save(download_file)
         download_file.seek(0)
